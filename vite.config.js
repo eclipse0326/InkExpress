36240600a82b64dd8f1996b861398651a89fda2e
eclipse0326 <858218411@qq.com>
2024-11-23 22:02:20 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+// vite.config.ts
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
